08b6a0c3487cf3918516633b033b5d428cf5acc4
Shreyas Soni <sonishreyas10@gmail.com>
2021-09-20 01:36:39 +0530

    Check hypotenuse done

---
+++ src/hypotenuse.js
@@ -0,0 +1,25 @@
+const sideALength = document.querySelector("#side-a");
+const sideBLength = document.querySelector("#side-b");
+const checkButton = document.querySelector("#check-hypotenuse");
+const output = document.querySelector("#output");
+
+function calculateHypotenuse(a,b)  {
+    return Math.sqrt(Math.pow(Number(a),2)+Math.pow(Number(b),2));
+}
+
+function checkHypotenuse() {
+    const a = sideALength.value;
+    const b = sideBLength.value;
+    if(a != "" && b != "") {
+        const hypotenus = calculateHypotenuse(a,b);
+        output.style.display="Block";
+        output.innerText = "Length of hypotenuse = " + String(hypotenus);
+    }   else{
+        output.style.display="Block";
+        output.innerText = "Please enter length of both sides";
+    }  
+}
+
+checkButton.addEventListener("click", checkHypotenuse);
+
+
