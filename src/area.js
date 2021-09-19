08160680c540f261639baf0cfb27640a1e18498b
Shreyas Soni <sonishreyas10@gmail.com>
2021-09-20 02:10:13 +0530

    check area done

---
+++ src/area.js
@@ -0,0 +1,25 @@
+const baseLength = document.querySelector("#base-length");
+const heightLength = document.querySelector("#height-length");
+const checkButton = document.querySelector("#check-area");
+const output = document.querySelector("#output");
+
+function calculateArea(b,h)  {
+    return (Number(b)*Number(h))/2;
+}
+
+function checkArea() {
+    const b = baseLength.value;
+    const h = heightLength.value;
+    if(b != "" && h != "") {
+        const area = calculateArea(b,h);
+        output.style.display="Block";
+        output.innerText = "Area of triangle = " + String(area) + " cm²";
+    }   else{
+        output.style.display="Block";
+        output.innerText = "Area of triangle = 0 cm²";
+    }  
+}
+
+checkButton.addEventListener("click", checkArea);
+
+
